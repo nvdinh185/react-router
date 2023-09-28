ee51bbe1f142585fedefe0d2fdc79dc190f0093e
nvdinh185 <nvdinh0766777123@gmail.com>
2023-09-28 14:15:04 +0700

    x

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, useParams } from "react-router-dom";
 
 function App() {
   return (
@@ -11,11 +11,15 @@ function App() {
           <li>
             <Link to="/about">About</Link>
           </li>
+          <li>
+            <Link to="/topic/123">Topic</Link>
+          </li>
         </ul>
 
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/about" element={<About />} />
+          <Route exact path="/topic/:topicId" element={<Topic />} />
         </Routes>
 
       </div>
@@ -29,6 +33,11 @@ function Home() {
 
 function About() {
   return <h2>About</h2>;
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>;
 }
 
 export default App;
